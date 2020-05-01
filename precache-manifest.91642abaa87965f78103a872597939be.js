8c34690c33bd30fa8ebdcdab76037737b5dc3662
Ibrahim Saberi <georgeipsum@gmail.com>
2020-04-30 20:15:23 -0400

    Updates

---
+++ precache-manifest.91642abaa87965f78103a872597939be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f02e81c653c1dc04ef742c76dbb4db",
+    "url": "/yello/index.html"
+  },
+  {
+    "revision": "e023dd022c7e3f2bbea7",
+    "url": "/yello/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "e29638ea1c53956614a8",
+    "url": "/yello/static/js/2.aa8e14be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yello/static/js/2.aa8e14be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e023dd022c7e3f2bbea7",
+    "url": "/yello/static/js/main.fe39a62b.chunk.js"
+  },
+  {
+    "revision": "be82fc2845084447eca6",
+    "url": "/yello/static/js/runtime-main.a8bd49cd.js"
+  }
+]);
