efcb5d84d8581dea6cedfa2e5d3adf197f13ba94
Ibrahim Saberi <georgeipsum@gmail.com>
2020-04-30 20:14:38 -0400

    deployment script

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div></div>
+    <div>deployment worked!</div>
   );
 }
 
